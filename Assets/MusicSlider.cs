5d8351f27a2f6fe5e02dedf181ce8185817fc08c
IronSensei <houchinmiller.lucas@gmail.com>
2019-03-18 20:45:58 -0400

    Fixed Slider consistency

---
+++ Assets/MusicSlider.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MusicSlider : MonoBehaviour
+{
+    public Slider music;
+    private AudioSource musicsource;
+
+    // Start is called before the first frame update
+    void Start()
+    { 
+        musicsource = GameObject.FindGameObjectWithTag("Music").GetComponent<AudioSource>();
+        music.value = musicsource.volume;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void SlideMusic()
+    {
+        musicsource.volume = GetComponent<Slider>().value;
+        PlayerPrefs.SetFloat("Chicken Volume", musicsource.volume);
+    }
+}
